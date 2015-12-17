fc4c25096c8e522afd87f6ac9bb64d9f1750fdd5
Baptiste Jamin <baptistejamin@gmail.com>
2015-12-17 17:53:11 +0100

    fill_map

--- srcs/ft_parser.c
+++ srcs/ft_parser.c
@@ -13,17 +13,31 @@
 #include <libft.h>
 #include "ft_fillit.h"
 
-int		ft_generate_map(t_env *env){
+int 	ft_fill_chars(char *str, int size){
 	int i;
 
 	i = 0;
-	env->map = (char **)malloc(sizeof(char *) * ft_sqrt(env->pieces_count * 4));
+	while (i < size){
+		str[i] = '.';
+		i++;
+	}
+	str[i] = '\0';
+	return (1);
+}
+int		ft_generate_map(t_env *env){
+	int i;
+	int map_size;
+
+	i = 0;
+	map_size = ft_sqrt(env->pieces_count * 4);
+	env->map = (char **)malloc(sizeof(char *) * map_size);
 	if (!env->map)
 		return (0);
-	while (i < ft_sqrt(env->pieces_count * 4)){
-		env->map[i] = (char *)malloc(sizeof(char) * ft_sqrt(env->pieces_count * 4));
+	while (i < map_size){
+		env->map[i] = (char *)malloc(sizeof(char) * (map_size + 1));
 		if (!env->map[i])
 			return (0);
+		ft_fill_chars(env->map[i], map_size + 1);
 		i++;
 	}
 	return (1);
